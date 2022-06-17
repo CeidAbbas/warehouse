dd3a88b28fd54a3ff26129f710060c395defb99c
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-06-17 10:52:50 +0430

    add person model

---
+++ src/main/java/ir/hinceid/warehouse/model/general/Person.java
@@ -0,0 +1,36 @@
+package ir.hinceid.warehouse.model.general;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class Person extends BaseClass {
+
+    @NotNull
+    @Column(name = "first_name")
+    public String firstName;
+
+    @NotNull
+    @Column(name = "last_name")
+    public String lastName;
+
+    @NotNull
+    @Column(name = "user_name")
+    public String username;
+
+    @NotNull
+    @Column(name = "password")
+    public String password;
+
+    @NotNull
+    @Column(name = "status")
+    public Boolean status;
+}
