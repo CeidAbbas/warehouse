85cbf550583c03e9fe618c80fc2697d237165952
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-06-17 10:51:37 +0430

    warehouse model, controller and repository

---
+++ src/main/java/ir/hinceid/warehouse/controller/WarehouseController.java
@@ -0,0 +1,34 @@
+package ir.hinceid.warehouse.controller;
+
+import ir.hinceid.warehouse.model.warhouse.Warehouse;
+import ir.hinceid.warehouse.repository.interfaces.IWarehouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/rest/warehouse/")
+public class WarehouseController {
+
+    @Autowired
+    private IWarehouseRepository iWarehouseRepository;
+
+    // getAll
+    @GetMapping("getAll")
+    public List<Warehouse> getAllWarehouses() {
+        return iWarehouseRepository.findAll();
+    }
+
+    // save or update
+    @PostMapping("save")
+    public Warehouse save(@RequestBody Warehouse warehouse) {
+        warehouse.setId(UUID.randomUUID());
+        warehouse.setCreatedDate(new Date());
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Person person = (Person) authentication.getPrincipal();
+        return iWarehouseRepository.save(warehouse);
+    }
+}
