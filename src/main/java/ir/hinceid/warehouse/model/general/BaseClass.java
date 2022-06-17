74d7463b347ae3dadc9794624dd49a709ec92ea2
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-06-17 10:52:56 +0430

    Create BaseClass.java

---
+++ src/main/java/ir/hinceid/warehouse/model/general/BaseClass.java
@@ -0,0 +1,34 @@
+package ir.hinceid.warehouse.model.general;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter @Setter
+public class BaseClass {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private UUID id;
+
+//    @NotNull
+    @Column(name = "created_date")
+    private Date createdDate;
+
+//    @NotNull
+    @Column(name = "created_user")
+    private UUID CreatedUser;
+}
