7244ff2cf11162f8fce48e9744454466733bc83d
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-06-17 10:52:26 +0430

    add ware model

---
+++ src/main/java/ir/hinceid/warehouse/model/warhouse/Ware.java
@@ -0,0 +1,37 @@
+package ir.hinceid.warehouse.model.warhouse;
+
+import com.sun.istack.NotNull;
+import ir.hinceid.warehouse.model.general.BaseClass;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ware")
+public class Ware extends BaseClass {
+
+    @NotNull
+    @Column(name = "name")
+    public String name;
+
+    @NotNull
+    @Column(name = "serial")
+    public String serial;
+
+    @NotNull
+    @Column(name = "contract_number")
+    public String contractNumber;
+
+    @NotNull
+    @Column(name = "expiration_date")
+    public String expirationDate;
+
+    @NotNull
+    @Column(name = "status")
+    public Boolean status;
+}
