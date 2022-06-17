0107a1a684b8168420a212ddbaa16b16ed21a375
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-06-18 01:31:23 +0430

    add warw class

---
+++ src/main/java/ir/hinceid/warehouse/controller/wareController.java
@@ -0,0 +1,32 @@
+package ir.hinceid.warehouse.controller;
+
+import ir.hinceid.warehouse.model.warhouse.Ware;
+import ir.hinceid.warehouse.repository.interfaces.IWareRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/rest/ware")
+public class wareController {
+
+    @Autowired
+    private IWareRepository iWarehouseRepository;
+
+    // getAll
+    @GetMapping("getAll")
+    public List<Ware> getAllWare() {
+        return iWarehouseRepository.findAll();
+    }
+
+    //save
+    @PostMapping("save")
+    public Ware saveWare(@RequestBody Ware ware) {
+        ware.setId(UUID.randomUUID());
+        ware.setCreatedDate(new Date());
+        return iWarehouseRepository.save(ware);
+    }
+}
