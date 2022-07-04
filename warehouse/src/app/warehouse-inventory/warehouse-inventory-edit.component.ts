53c0a9ce02178e6d9d11c4d02fa808bce787630b
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-07-04 19:23:25 +0430

    initial

---
+++ warehouse/src/app/warehouse-inventory/warehouse-inventory-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-inventory-edit',
+  templateUrl: './warehouse-inventory-edit.component.html',
+  styleUrls: ['./warehouse-inventory-edit.component.css']
+})
+export class WarehouseInventoryEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
