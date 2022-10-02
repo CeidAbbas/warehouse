bdab5f0256f1385ad7263995e61e897b52a04cf1
Ce-iD <34376449+Ce-iD@users.noreply.github.com>
2022-10-02 23:14:57 +0330

    commit

--- src/main/java/ir/hinceid/warehouse/model/warhouse/WarehouseInventory.java
+++ src/main/java/ir/hinceid/warehouse/model/warhouse/WarehouseInventory.java
@@ -2,6 +2,8 @@ package ir.hinceid.warehouse.model.warhouse;
 
 //import com.sun.istack.NotNull;
 import ir.hinceid.warehouse.model.general.BaseModel;
+import ir.hinceid.warehouse.model.general.Person;
+import ir.hinceid.warehouse.model.references.BaseInformation;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,21 +18,54 @@ import javax.persistence.Table;
 @Table(name = "warehouseInventory")
 public class WarehouseInventory extends BaseModel {
 
-//    @NotNull
-    @ManyToOne
-    @JoinColumn(name = "ware_id")
-    public Ware ware;
-
-//    @NotNull
+    //    @NotNull
     @ManyToOne
     @JoinColumn(name = "warehouse_id")
     public Warehouse warehouse;
 
-    public Integer inventory;
+    //    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "ware_id")
+    public Ware ware;
 
-    public String expirationDate;
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    public Person producer;
+
+    @ManyToOne
+    @JoinColumn(name = "forwarding_id")
+    public Forwarding forwarding;
+
+    @ManyToOne
+    @JoinColumn(name = "reference_id")
+    public WarehouseInventory reference;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_healthy_status_id")
+    public BaseInformation warehouseHealthyStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_healthy_status_id")
+    public BaseInformation deliveryHealthyStatus;
+    @ManyToOne
+    @JoinColumn(name = "buy_origin_id")
+    public BaseInformation buyOrigin;
+
+    public String entryDate;
+
+    public String exitDate;
 
     public String serial;
 
     public String contractNumber;
+
+    public String barcode;
+
+    public String description;
+
+    public String color;
+
+    public String expirationDate;
+
+    public String appurtenance;
 }
